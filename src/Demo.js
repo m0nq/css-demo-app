558a6d83c67ca44a80a712ffed3d1cae8ea76bc3
Monk Wellington <forgetmenaught@gmail.com>
2019-07-26 12:49:16 -0700

    Selected elements with various levels of specificity

--- src/Demo.js
+++ src/Demo.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './Demo.css';
 
 export const Demo = props => {
   // const styles = { color: props.textColor, textDecoration: 'underline' };
   //
   // return <div style={styles}>{props.children}</div>;
-  return <div>{props.children}</div>;
+  return <Fragment>
+    <div>{props.children}</div>
+    <div className="info">{props.children}</div>
+    <div id="message">{props.children}</div>
+    <section className="info">{props.children}</section>
+  </Fragment>;
 };
